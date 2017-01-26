4040dcdd460b55eebed6cf01320d890aeea3dd62
BlaneTorregrossa <blanet97@cox.net>
2017-01-26 08:59:06 -0600

    .

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -13,19 +13,19 @@ namespace CSharpPractice
         public Entity(int h, int att) { }
 
         private int m_health = 50;                  // Private Member Variable of type int name of m_health
-        public int Health                           // Public Varible of type int name of Health
+        protected int Health                           // Public Varible of type int name of Health
         {
             get { return m_health; }                // Returns member variable of type int m_health
             set { value = m_health; }               // Sets the value of Public variable Health to equal the value of private variable m_health
         }
 
         private int m_attack = 5;                   // Private Member Variable of type int name of m_attack
-        public int Attack                           // Public Variable of type int name of Attack
+        protected int Attack                           // Public Variable of type int name of Attack
         {
             get { return m_attack; }                // Returns member variable of type int m_attack
             set { value = m_attack; }               // Sets the value of Public variable Attack to equal the value of private variable m_attack
         }
-        public virtual bool Attack(Entity);         //*
+        public virtual bool Attack();               //*
 
         private string m_name = "Bob";              // Private Member Variable of type string name of m_name
         public string Name                          // Public Variable of type string name of Name
@@ -39,7 +39,7 @@ namespace CSharpPractice
 
             public Zombie() { }                     // Default Constructor for Zombie
             public Zombie(int a, int b) { }         
-            public override Attack(Entity);         //*
+            public override Attack();               //*
 
 
         }
@@ -49,7 +49,7 @@ namespace CSharpPractice
 
             public Player() { }                    // Default Constructor for Player
             public Player(int a, int b) { }        
-            public override Attack(Entity);        //*
+            public override Attack();              //*
             public Defend();                       //*
         }
     }
---
+++ WinFormPracticeNonAssignment/DataManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WinFormPracticeNonAssignment
+{
+    class DataManagement
+    {
+
+
+        public static void Serialize()
+        {
+
+           
+
+        }
+
+        public void Deserialize()
+        {
+
+
+
+        }
+
+    }
+}
